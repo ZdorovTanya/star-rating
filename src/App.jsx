99efdc9d1ec18a1d6379a1262c18c18b44af4127
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-07-26 13:52:11 +0300

    use callback and switch for user mood

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import './App.css';
-import StarRating from './StarRating';
+import React from "react";
+import "./App.css";
+import StarRating from "./StarRating";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const handleChange = (value) => {
+    setValue(value);
+  };
+
+  const UserMood = ({ val }) => {
+    switch (val) {
+      case 1:
+        return <span>need to cry</span>;
+      case 2:
+        return <span>need to eat candy</span>;
+      case 3:
+        return <span>need to take a walk</span>;
+      case 4:
+        return <span>are an optimist</span>;
+      case 5:
+        return <span>ate lying</span>;
+
+      default:
+        return <span>think...</span>;
+    }
+  };
+
   return (
     <div className="App">
-      <StarRating/>
+      <StarRating onChange={handleChange} />
+      {/* <h2>{value < 3 ? "need to cry" : null}</h2> */}
+      <span>
+        You <UserMood val={value} />
+      </span>
     </div>
   );
 }
--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-const StarRating = () => {
+const StarRating = ({ onChange }) => {
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
 
+  const handleChange = (ratingValue) => {
+    onChange(ratingValue); // callback-функция
+  };
+
   return (
-    <div className="">
+    <div className="wrapper">
+      <h1>How do you rate your mood:</h1>
       {[...Array(5)].map((star, i) => {
         const ratingValue = i + 1;
         return (
-          <label htmlFor=""  key={ratingValue}>
+          <label htmlFor="" key={ratingValue}>
             <input type="radio" name="rating" value={ratingValue} />
             <FaStar
               className="star"
-              onClick={() => setRating(ratingValue)}
+              onClick={() => {
+                setRating(ratingValue);
+                handleChange(ratingValue);
+              }}
+              //   onChange={handleChange}
               onMouseEnter={() => setHover(ratingValue)}
               onMouseLeave={() => setHover(null)}
               color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
